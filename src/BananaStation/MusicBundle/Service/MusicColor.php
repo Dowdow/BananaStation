89a5c2d2e3d25ec2e2f679eccfe5f139eb007e2f
Léo Riera <leo.riera1@gmail.com>
2016-10-17 19:54:18 +0200

    Add theme color music

--- src/BananaStation/MusicBundle/Service/MusicColor.php
+++ src/BananaStation/MusicBundle/Service/MusicColor.php
@@ -9,10 +9,38 @@ class MusicColor extends \Twig_Extension
     {
         return array(
             new \Twig_SimpleFilter('mcolor', array($this, 'musicColor')),
+            new \Twig_SimpleFilter('mcolorclass', array($this, 'musicColorClass')),
         );
     }
 
     public function musicColor($style)
+    {
+        switch ($style) {
+            case 'G':
+                $style = '#ff4136';
+                break;
+            case 'H':
+                $style = '#ff851b';
+                break;
+            case 'M':
+                $style = '#ffdc00';
+                break;
+            case 'E':
+                $style = '#0074d9';
+                break;
+            case 'D':
+                $style = '#2ecc40';
+                break;
+            case 'R':
+                $style = '#aaaaaa';
+                break;
+            case 'S':
+                $style = '#39cccc';
+        }
+        return $style;
+    }
+
+    public function musicColorClass($style)
     {
         switch ($style) {
             case 'G':
